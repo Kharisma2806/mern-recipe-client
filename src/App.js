85a0b63419e40fd741ef743cbcea3f4bbd3b9900
kharisma2806 <kiran.nankani28@gmail.com>
2024-04-30 20:49:49 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/navbar";
+import { Auth } from "./pages/saved-recipes"; // Importing Auth component from saved-recipes.js
+import { CreateRecipe } from "./pages/create-recipe";
+import { Home } from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<Auth />} />{" "}
+          {/* Rendering Auth component */}
+          <Route path="/auth" element={<Auth />} />{" "}
+          {/* Rendering Auth component */}
+        </Routes>
+      </Router>
     </div>
   );
 }
